66a6879047abd5b8f29438700206cb899a9345a3
Gonçalo Monteiro de Carvalho da Costa Santos <goncalocostasantos@MBP-de-Goncalo.lan>
2019-10-10 20:29:30 +0100

    added the beginning with html partially done and starting adding JS

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -2,17 +2,22 @@ function deleteItem(e){
 
 }
 
-function getPriceByProduct(itemNode){
-
+function getPriceByProduct(){
+  
 }
+
 
 function updatePriceByProduct(productPrice, index){
 
 }
 
 function getTotalPrice() {
+  let num1 = Number(document.getElementById('unit-price').value);
+  let num2 = Number(document.getElementById('input-number').value);
 
+  console.log();
 }
+
 
 function createQuantityInput(){
 
@@ -38,15 +43,15 @@ function createNewItem(){
 
 }
 
-window.onload = function(){
+window.addEventListener('load' ,function() {
   var calculatePriceButton = document.getElementById('calc-prices-button');
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
   calculatePriceButton.onclick = getTotalPrice;
-  createItemButton.onclick = createNewItem;
+  //createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
-};
+});
